5a6f5bc0157ff26b5de8139f6f55096a50da2c1c
AKJ <kjajila@gmail.com>
2023-01-31 22:42:40 -0500

    add event listners and radio buttons for project4

--- assets/js/rockPaperScissorGame.js
+++ assets/js/rockPaperScissorGame.js
@@ -1,4 +1,5 @@
-//global variables
+let selectBtn=document.getElementById("selectBtn");
+
 var option="RPS";
 var scoreCard={
      tie: 0,
@@ -6,63 +7,78 @@ var scoreCard={
      computer: 0
 };
 
-
-//checking whether the player want to conitnue? If yes start the game again
-// else display the final scoreboard   
+// display the final scoreboard   
 function finalScore(){
-     var cont=confirm("Do you want to continue?")
-     if(cont)
-     {
-          startGame();
-     }
-     else{
-          //alert("This is the final score card: You won "+scoreCard.user+ " times and computer won "+scoreCard.computer+" times and "+scoreCard.tie+" ties.");
-          document.getElementById("user").textContent="You won "+(scoreCard.user)+ " times";
-          document.getElementById("computer").textContent="Computer won "+(scoreCard.computer)+ " times";
-          document.getElementById("tie").textContent="There were "+(scoreCard.tie)+ " ties";
-     }
+     document.getElementById("userWon").textContent="You won "+(scoreCard.user)+ " times";
+     document.getElementById("computerWon").textContent="Computer won "+(scoreCard.computer)+ " times";
+     document.getElementById("tie").textContent="There were "+(scoreCard.tie)+ " ties";
 }
 
-//checking user input includes r/p/s
-function isValid(inputFromUser){
-     if(inputFromUser==="R" || inputFromUser==="P" || inputFromUser==="S"){
-          return true;
-     }
-     else{
-          startGame();
-     }
-}
-
-//taking user input and checking it against computer's random selection
+// taking user input and checking it against computer's random selection
 function startGame(){
-     var userInput=prompt("Type 'r' for Rock:  Type 's' for scissors: Type 'p' for paper: Which one you want to select?");
-     if(userInput===null){
-          startGame();
+     let userRock=document.getElementById("userRock").checked;
+     let userPaper=document.getElementById("userPaper").checked;
+     let userScissor=document.getElementById("userScissor").checked;
+     let computerRock=document.getElementById("computerRock");
+     let computerPaper=document.getElementById("computerPaper");
+     let computerScissor=document.getElementById("computerScissor");
+     let result=document.getElementById("result");
+     let error=document.getElementById("error");
+     let newListEl=document.createElement("li");
+     let text;
+     let validInput=true;
+     error.textContent="";
+     if(userRock){
+          userInput="R";
+     }
+     else if(userPaper){
+          userInput="P";
+     }
+     else if(userScissor){
+          userInput="S";
      }
      else{
-          userInput=userInput.toUpperCase();
-          if(isValid(userInput)){
-               var rand=Math.floor(Math.random()*(option.length));
-               var computer=option[rand];
-               alert("Computer selected: "+computer);
-               if(userInput===computer){
-                    scoreCard.tie++;
-                    alert("It is a tie");
-               }
-               else if((userInput==='R' && computer==='S')||(userInput==='S' && computer==='P') ||(userInput==='P' && computer==='R')){
-                    scoreCard.user++;
-                    alert("You WON!!!");
-               }
-               else{
-                    scoreCard.computer++;
-                    alert("SORRY you lost");
-               }
-               finalScore();
-          }      
-          
+          error.textContent="Please select Rock / Paper / Scissor";
+          validInput=false;
+     }
+     if(validInput){
+          var rand=Math.floor(Math.random()*(option.length));
+          var computer=option[rand];
+     
+          if(computer==="R"){
+               computerRock.checked=true;
+          }
+          else if(computer==="P"){
+               computerPaper.checked=true;
+          }
+          else if(computer==="S"){
+               computerScissor.checked=true;
+          }
+          if(userInput===computer){
+               scoreCard.tie++;
+               text="It is a tie";
+          }
+          else if((userInput==='R' && computer==='S')||(userInput==='S' && computer==='P') ||(userInput==='P' && computer==='R')){
+               scoreCard.user++;
+               text="You WON!!!";
+          }
+          else{
+               scoreCard.computer++;
+               text="SORRY you lost";
+          }
+     
+          newListEl.textContent=text+`(You selected ${userInput} and computer selected ${computer})`;  
+          result.appendChild(newListEl);
+          finalScore(); 
      }   
-}
+}   
 
 
-//function call
-startGame();
+//function call button event listner
+selectBtn.addEventListener("click",startGame);
+
+document.getElementById("restartBtn").addEventListener("click",function(){
+     document.location.reload()
+});
+
+
